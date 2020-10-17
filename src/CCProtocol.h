c69025d7a87701c3ba876562a1a3cebe08a21a56
NNpanpan <dpnhat.nguyen@gmail.com>
2020-10-18 00:31:57 +0800

    prototyping CC protocols 1

--- src/CCProtocol.h
+++ src/CCProtocol.h
@@ -1,14 +1,18 @@
 #ifndef CCPROTOCOL_H
 #define CCPROTOCOL_H
 
+#include "Cache.h"
+
 /*
 Class CCProtocol:
     - Interface for cache coherence protocols
 */
 
 class CCProtocol {
-// public:
-//     virtual 
+public:
+    virtual void nextState(const Cache* cache) = 0;
+    virtual void prRd(const Cache* cache, const int & addr) = 0;
+    virtual void prWr(const Cache* cache, const int & addr) = 0;
 };
 
 #endif
---
+++ src/MESI.h
@@ -0,0 +1,18 @@
+#ifndef MESICC_H
+#define MESICC_H
+
+#include "CCProtocol.h"
+
+/*
+Class MESI:
+    - Implements MESI cache coherence protocol
+*/
+
+class MESI: public CCProtocol {
+public:
+    void nextState(const Cache* cache);
+    void prRd(const Cache* cache, const int & addr);
+    void prWr(const Cache* cache, const int & addr);
+};
+
+#endif
